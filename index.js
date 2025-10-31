43cf87ec386b6f3d531a7e37e4743d596b7a1e5b
Ajeshjose11 <ajeshjose11@gmail.com>
2025-10-31 07:33:11 +0530

    Completed

---
+++ index.js
@@ -0,0 +1,15 @@
+
+const jsonServer = require('json-server');
+
+const server = jsonServer.create();
+const router = jsonServer.router('server/db.json');
+const middleware = jsonServer.defaults();
+
+server.use(middleware);
+server.use(router);
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`JSON Server running on port: ${PORT}`);
+});
