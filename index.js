397ca91c4e5b98cad175438bfe88cf1476d1ef21
Ajeshjose11 <ajeshjose11@gmail.com>
2025-11-23 09:51:58 +0530

    error resolved

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const jsonServer = require('json-server');
-const path = require('path')
+// const path = require('path')
 
 const server = jsonServer.create();
 const middleware = jsonServer.defaults();
-const router = jsonServer.router(path.join(__dirname, 'db.json'));
-// const router = jsonServer.router('db.json');
+// const router = jsonServer.router(path.join(__dirname, 'db.json'));
+const router = jsonServer.router('db.json');
 
 server.use(middleware);
 server.use(router);
